89bd353f8ccbf154012369a94541b3a4061dc02c
crusherblack <fadhildarma13@gmail.com>
2023-05-29 20:12:55 +0700

    chore: adjust footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,15 +2,20 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-white rounded-lg shadow dark:bg-black/80 -mt-36 dark:text-white/90">
+    <footer className="relative bg-white rounded-lg shadow dark:bg-black/80 -mt-36 dark:text-white/90">
       <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-900 lg:my-8" />
-      <span className="block text-sm text-gray-500 text-center dark:text-gray-400 pb-8">
-        © 2023{" "}
-        <a href="#" className="hover:underline">
-          Fadhil Darma Putera Zagoto
+      <div className="block text-center text-sm text-gray-500  dark:text-gray-400 pb-8">
+        <p>© 2023 Fadhil Darma Putera Zagoto. All Rights Reserved.</p>
+
+        <a
+          href="https://github.com/crusherblack/github-repositories-explorer"
+          className="hover:cursor-pointer hover:underline text-black/90 dark:text-white/90 "
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {`(Open Source on Github)`}
         </a>
-        . All Rights Reserved.
-      </span>
+      </div>
     </footer>
   );
 };
